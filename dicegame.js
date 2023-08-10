ad4f9d29a88eb745810ee3b8821ad2a6ae942e4f
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-08-09 23:54:18 -0400

    dice-faces

---
+++ dicegame.js
@@ -0,0 +1,6 @@
+function rolldice () {
+  var rand1=Math.round(Math.random() * 5) + 1;
+  var rand2=Math.round(Math.random() * 5) + 1;
+  var rand3=Math.round(Math.random() * 5) + 1;
+  var rand4=Math.round(Math.random() * 5) + 1;
+}
