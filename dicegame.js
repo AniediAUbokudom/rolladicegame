b5cc5b79951dc1920ec058b958c0ddc4baa2e1d0
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-08-10 23:46:45 -0400

    first try of diceRoll

--- dicegame.js
+++ dicegame.js
@@ -1,6 +1,12 @@
 function rolldice () {
-  var rand1=Math.round(Math.random() * 5) + 1;
-  var rand2=Math.round(Math.random() * 5) + 1;
-  var rand3=Math.round(Math.random() * 5) + 1;
-  var rand4=Math.round(Math.random() * 5) + 1;
+  var randNum1=Math.round(Math.random() * 5) + 1;
+  var randNum2=Math.round(Math.random() * 5) + 1;
+  var randNum3=Math.round(Math.random() * 5) + 1;
+  var randNum4=Math.round(Math.random() * 5) + 1;
+
+  document.getElementById("playerOneDice1").src="dice-faces/u" + randNum1 + ".jpg";
+  document.getElementById("playerOneDice2").src="dice-faces/u" + randNum2 + ".jpg";
+  document.getElementById("playerTwoDice1").src="dice-faces/e" + randNum3 + ".png";
+  document.getElementById("playerOneDice2").src="dice-faces/e" + randNum4 + ".png";
 }
+
